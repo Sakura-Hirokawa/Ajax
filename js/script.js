03831d010dbcba999063a75694eaf97bb3f5c453
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-04-27 11:32:04 +0900

    [Add]js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-$(fuction(){
-  $('#btn').on('click',function(){
+API_KEY = "899e0cdbf0b3ab3bf2f6809f00098e23"
 
+$(function(){
+  $('#btn').on('click',function(){
+    $ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType:'jsonp',
+    }).done(function (data){
+    }).fail(function (data){
+      
+    })
   });
 });
